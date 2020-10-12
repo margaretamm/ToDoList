6ae1a12e02bede8ed78eb53f87e38682dbc79dec
margaretamm <meggy.mudrikova@gmail.com>
2020-10-12 12:14:08 +0200

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Margareta Mudrikova on 12/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
